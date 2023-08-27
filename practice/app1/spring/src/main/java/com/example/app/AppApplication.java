b87036a9a38e61f7c725daa76873afa870eed63f
mitani1993 <fytim55@gmail.com>
2023-08-27 21:37:49 +0900

    springbootサンプル

---
+++ practice/app1/spring/src/main/java/com/example/app/AppApplication.java
@@ -0,0 +1,21 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class AppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String hello() {
+    return "Hello Spring Boot on Docker";
+  }
+}
